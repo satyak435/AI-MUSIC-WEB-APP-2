fe9ea2a01fbe06f8af6236712c8137f8f12e0c1e
satyak435 <137170193+satyak435@users.noreply.github.com>
2024-03-11 16:27:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+song="";
+function preload(){
+    song=loadSong("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    
+}
